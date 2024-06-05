2c9f693ab049da036cae97bbefd2bc5246204497
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-06-06 01:50:33 +0200

    Updated server code

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from "express";
+import fetchJson from "./helpers/fetch-json.js"
+
+const app = express();
+
+// main HTML files and the public files
+app.set("views", "./views");
+app.use(express.static("./public/"));
+
+// ejs
+app.set("view engine", "ejs");
+
+// dont know what this does
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ports //
+app.set("port", process.env.PORT || 8777);
+
+app.listen(app.get("port"), function(){
+	console.log(`Test link ${app.get("port")}`);
+	console.log("Server listening on port " + '👉 ' + `http://localhost:${app.get("port")}`+ ' 👈');
+});
