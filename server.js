3f702ccc3123a4a3e63854e7192d445bcd0f3e6b
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-06-11 13:36:21 +0200

    server, homepage & brew routes
    - established the server
    - created the firs routes for the project

--- server.js
+++ server.js
@@ -13,11 +13,74 @@ app.set("view engine", "ejs");
 // dont know what this does
 app.use(express.urlencoded({ extended: true }));
 
+// merlin book of potion api 
+
+const apiUrl = "https://potion-api-jet.vercel.app/";
+
+// all ingridients endpoint
+
+const IngrUrl = `${apiUrl}ingredients`;
+
+const potionsUrl = `${apiUrl}potions`;
+
+// new postions storage
+
+let archive = app.locals.storage = [];
+
+// page refresh counter
+var bodycounter = app.locals.visitCounter = 0;
+
+
+// homepage 
+app.get("/", (req, res) => {
+	Promise.all([
+		fetchJson(IngrUrl),
+		fetchJson(potionsUrl),
+	])
+		.then(([ingredientsData, potionsData]) => {
+			res.render("index.ejs", { ingredients : ingredientsData, potions : potionsData });
+			bodycounter++
+
+			console.log("home success",	bodycounter	);
+		})
+		.catch(err => alert("home failed"));
+});
+
+app.get("/brew/1", (req, res) => {
+	Promise.all([
+		fetchJson(IngrUrl),
+		fetchJson(potionsUrl),
+	])
+		.then(([ingredientsData, potionsData]) => {
+			res.render("brew.ejs", { ingredients : ingredientsData, potions : potionsData });
+			bodycounter++
+
+			console.log("page1 success",	bodycounter	);
+		})
+		.catch(err => alert("home failed"));
+})
+
+app.post("/brew/1",(req,res) =>{
+	fetchJson(potionsUrl).then((potions) => {
+	const {description,ingredients} = req.body;
+
+	
+
+	// if (!Array.isArray(ingredients)) {
+	// 	return response.status(400).send('nop');
+	//   }
+	res.render("brew.ejs",{description})
+
+	})
+})
 
 
 
+// app.post('/brew/1', function(req,res){
+// 	fetchJson(potionsUrl).then((potions) => {
 
-
+// 	})
+// })
 
 
 
